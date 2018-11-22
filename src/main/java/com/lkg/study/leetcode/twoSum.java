06eb3a8338e0529a97d7329d26c4b12ffedf0d2e
lkg <627861816@qq.com>
2018-11-22 13:47:42 +0800

    9   回文数

--- src/main/java/com/lkg/study/leetcode/twoSum.java
+++ src/main/java/com/lkg/study/leetcode/twoSum.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 import java.util.HashMap;
 
 /**
+ * 	1   两数之和
+ *
  * 给定一个整数数组和一个目标值，找出数组中和为目标值的两个数。
  *
  * 你可以假设每个输入只对应一种答案，且同样的元素不能被重复利用。
@@ -14,7 +16,7 @@ import java.util.HashMap;
  * 因为 nums[0] + nums[1] = 2 + 7 = 9
  * 所以返回 [0, 1]
  */
-public class Solution {
+public class twoSum {
 
     public static void main(String[] args){
         int[] nums = {2, 7, 11, 15};
